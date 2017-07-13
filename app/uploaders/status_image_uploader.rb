e83991535598f62c886f6800e0727544115e917f
Mario Zugaj <mario@mariozugaj.com>
2017-07-13 15:37:07 +0200

    Remove StatusUpdateImageUploader default_url

--- app/uploaders/status_image_uploader.rb
+++ app/uploaders/status_image_uploader.rb
@@ -6,8 +6,4 @@ class StatusImageUploader < CarrierWave::Uploader::Base
   version :normal do
     process resize_to_limit: [840, 420]
   end
-
-  def default_url(*args)
-    'https://s3.eu-central-1.amazonaws.com/chanjman-peoplebook/missing/status/missing.jpg'
-  end
 end
