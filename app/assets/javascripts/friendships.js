a9999b4b442c766a8497973e2b866492bbb5226a
Mario Zugaj <mario@mariozugaj.com>
2017-07-16 15:49:36 +0200

    Refactor friendship.js not to change html

--- app/assets/javascripts/friendships.js
+++ app/assets/javascripts/friendships.js
@@ -4,21 +4,23 @@ PeopleBook.FriendshipsModule = (function () {
 
   var init = function () {
     _friendshipButtonListener();
-    _friendCardDimmer();
   };
 
   var _friendshipButtonListener = function () {
     $('.friendship_button').on('mouseenter mouseleave', function (e) {
-      var container = $(this).find('.unfriend');
-      var friends = "<i class='checkmark icon'></i><span>Friends</span>";
-      var unfriend = "<i class='remove user icon'></i><span>Unfriend</span>";
-      container.html(e.type == 'mouseenter' ? unfriend : friends);
-    });
-  };
+      var $container = $(this).find('.unfriend');
+      var $icon = $container.children('i');
+      var $buttonText = $container.children('span');
 
-  var _friendCardDimmer = function () {
-    $('.raised.card .image, .ui.small.images .image').dimmer({
-      on: 'hover',
+      if (e.type == 'mouseenter') {
+        $icon.removeClass('checkmark');
+        $icon.addClass('remove user');
+        $buttonText.text('Unfriend');
+      } else if (e.type == 'mouseleave') {
+        $icon.removeClass('remove user');
+        $icon.addClass('checkmark');
+        $buttonText.text('Friends');
+      }
     });
   };
 
