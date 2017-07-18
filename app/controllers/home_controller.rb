b7eac8a802929fd163f3d50fed3a23592d3dcce2
Mario Zugaj <mario@mariozugaj.com>
2017-07-18 19:04:21 +0200

    Paginate home page status updates

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,12 @@
 class HomeController < ApplicationController
   def index
     @new_status_update = StatusUpdate.new
-    @feed = StatusUpdate.includes([author: :profile], comments: [author: :profile])
-                        .where("author_id IN (:friends_ids) OR author_id = :user_id",
-                               friends_ids: current_user.friends_ids,
-                               user_id: current_user.id)
-                        .order(created_at: :desc)
+    @feed =
+      StatusUpdate.includes([author: :profile], comments: [author: :profile])
+                  .where('author_id IN (:friends_ids) OR author_id = :user_id',
+                         friends_ids: current_user.friends_ids,
+                         user_id: current_user.id)
+                  .order(created_at: :desc)
+                  .page params[:page]
   end
 end
