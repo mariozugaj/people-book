089d0665f4ad7bc528a1bd0cde30e6fad178fe44
Mario Zugaj <mario@mariozugaj.com>
2017-08-02 09:39:49 +0200

    Ask present? or blank? or exists? instead of any?

--- app/decorators/photo_album_decorator.rb
+++ app/decorators/photo_album_decorator.rb
@@ -2,7 +2,7 @@ class PhotoAlbumDecorator < Draper::Decorator
   delegate_all
 
   def first_image
-    return object.images.first.image.url(:mini) if object.images.any?
+    return object.images.first.image.url(:mini) if object.images.exists?
     PhotoAlbum::DEFAULT_IMAGE
   end
 end
