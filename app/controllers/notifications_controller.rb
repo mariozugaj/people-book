9292334a190b9b8bce812a25cebd595228d808ea
Mario Zugaj <mario@mariozugaj.com>
2017-08-02 17:05:28 +0200

    Refactor search input and search index

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,8 +1,9 @@
 class NotificationsController < ApplicationController
   def index
-    @notifications = Notification.where(recipient: current_user).recent
-    @all_notifications = Notification.where(recipient: current_user)
-                                     .order(created_at: :desc)
+    @notifications ||= Notification.where(recipient: current_user).recent
+    @all_notifications ||= Notification.where(recipient: current_user)
+                                       .order(created_at: :desc)
+                                       .page(params[:page])
 
   end
 
