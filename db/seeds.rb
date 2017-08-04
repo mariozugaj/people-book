1a674757c497897d5bb1f7a1e3d4ad9659dbb716
Mario Zugaj <mario@mariozugaj.com>
2017-08-04 14:42:50 +0200

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,27 +8,13 @@ puts 'done!'
 
 # Multiplier constant
 
-MULTIPLIER = 50
+MULTIPLIER = 10
 
 # Users
 
 print 'Creating users...'
 
-MULTIPLIER.times do |index|
-  first_name = Faker::Name.first_name
-  last_name = Faker::Name.last_name
-  User.create!(name: "#{first_name} #{last_name}",
-               email: Faker::Internet.email(first_name),
-               password: Faker::Internet.password(30, 50))
-end
-
-print "#{User.count} created..."
-puts 'done!'
-
 # Create user profiles with about field as a quote from API
-
-print 'Creating profiles...'
-
 RELATIONSHIP_OPTIONS = ['Single',
                         'In a relationship',
                         'Engaged',
@@ -43,68 +29,72 @@ RELATIONSHIP_OPTIONS = ['Single',
 
 require 'rest-client'
 
-User.all.each do |user|
-
-  # Get quote from Mashape and parse it
-  response =
-    RestClient.get('https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous&count=1',
-      headers = {
-                  'X-Mashape-Key' => Figaro.env.MASHAPE_KEY,
-                  'Accept' => 'application/json'
-                })
-  quote = JSON.parse(response.body)['quote']
-
-  Profile.create(user: user)
-
-  user.reload
-      .profile
-      .update(birthday: Faker::Date.between(60.years.ago, 18.years.ago),
-              education: Faker::University.name,
-              hometown: Faker::Address.city,
-              profession: Faker::Job.title,
-              company: Faker::Company.name,
-              relationship_status: RELATIONSHIP_OPTIONS[rand(9)],
-              phone_number: Faker::PhoneNumber.cell_phone,
-              about: quote)
+# Get quote from Mashape and parse it
+QUOTES = Array.new(MULTIPLIER) do
+  JSON.parse(RestClient.get('https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous&count=1',
+                            'X-Mashape-Key' => Figaro.env.MASHAPE_KEY,
+                            'Accept' => 'application/json')
+      .body)['quote']
 end
 
-print "#{Profile.count} created..."
-puts 'done!'
-
-# Create random no of friend requests to random users from each user
-
-print 'Creating friend requests...'
-
-User.all.each do |user|
-  # Get random no of user ids which are != user id and
-  # from which user hasn't received friend request
-  random_friend_ids = User.pluck(:id)
-                          .reject { |id| id == user.id }
-                          .reject do |id|
-                            user.received_friend_requests
-                                .map(&:user_id)
-                                .include? id
-                          end
-                          .sample(rand((MULTIPLIER * 0.5)..MULTIPLIER))
-
-  # Create friend request for each random user id
-  random_friend_ids.each do |id|
-    user.sent_friend_requests.create(friend: User.find(id))
-  end
+PROFILES = Array.new(MULTIPLIER) do |idx|
+  {
+    birthday: Faker::Date.between(60.years.ago, 18.years.ago),
+    education: Faker::University.name,
+    hometown: Faker::Address.city,
+    profession: Faker::Job.title,
+    company: Faker::Company.name,
+    relationship_status: RELATIONSHIP_OPTIONS[rand(9)],
+    phone_number: Faker::PhoneNumber.cell_phone,
+    about: QUOTES[idx]
+  }
 end
-print "#{Friendship.count} created..."
-puts 'done!'
+
+USERS = Array.new(MULTIPLIER) do |idx|
+  name = Faker::Name.name
+  {
+    name: name,
+    email: Faker::Internet.email(name),
+    password: Faker::Internet.password(30, 50),
+    profile: PROFILES[idx]
+  }
+end
+
+USERS.each do |user|
+  new_user = User.new(name: user[:name],
+                      email: user[:email],
+                      password: user[:password])
+  new_user.build_profile(user[:profile])
+  new_user.save
+end
+
+puts "#{User.count} created!"
+
+# Create friendships
+
+print 'Creating friendships...'
+
+FRIENDSHIPS = User.pluck(:id)
+                  .repeated_combination(2)
+                  .reject { |combination| combination[0] == combination[1] }
+                  .sample((MULTIPLIER**2) * 0.7)
+                  .map do |combination|
+                    {
+                      user_id: combination[0],
+                      friend_id: combination[1]
+                    }
+                  end
+
+FRIENDSHIPS.each { |friendship| Friendship.create(friendship) }
 
 # Accept random no of friend requests
-
-print 'Accepting friend requests...'
-
+friendship_count = Friendship.count
 Friendship.all
-          .sample(rand((Friendship.count * 0.5)..Friendship.count))
+          .sample(rand((friendship_count * 0.7)..friendship_count))
           .each { |friendship| friendship.update(accepted: true) }
 
-print "#{Friendship.where(accepted: true).count} accepted..."
-puts 'done!'
+puts "#{Friendship.where(accepted: true).count} created!"
+
 
 # Create 3..10 status updates for each user, pulled as articles from Wikipedia
 
@@ -113,7 +103,7 @@ require 'wikipedia'
 print 'Creating status updates...'
 
 User.all.each do |user|
-  rand(3..10).times do
+  1.times do
     # Get random article from Wikipedia
     status_update_text = ''
     article_image = ''
@@ -122,7 +112,7 @@ User.all.each do |user|
       article = Wikipedia.find_random
       if !article.summary.nil? && !article.images.nil? && (article.summary.size > 1000)
         status_update_text = "#{article.title}\n\n#{article.summary}\n\nRead more <a href='#{article.fullurl}'>here</a>"
-        article_image = article.image_urls.shuffle.first
+        article_image = article.image_urls.sample
       end
       break if !article.summary.nil? && !article.images.nil? && (article.summary.size > 1000)
     end
@@ -132,8 +122,7 @@ User.all.each do |user|
   end
 end
 
-print "#{StatusUpdate.count} created..."
-puts 'done!'
+puts "#{StatusUpdate.count} created!"
 
 # Create default photo albums + highlights album with 5 random images and
 # update profile avatar and cover photo + put images into albums
@@ -145,8 +134,8 @@ User.all.each do |user|
   cover_photos = PhotoAlbum.create(author: user, name: 'Cover photos')
   highlights = PhotoAlbum.create(author: user, name: 'Highlights')
 
-  avatars.images.create(remote_image_url: Faker::LoremPixel.image('780x720', false, 'nightlife'))
-  cover_photos.images.create(remote_image_url: Faker::LoremPixel.image('1260x630', false, 'nature'))
+  avatars.images.create(remote_image_url: Faker::LoremPixel.image('780x720', false))
+  cover_photos.images.create(remote_image_url: Faker::LoremPixel.image('1260x630', false))
 
   user.profile.update avatar: avatars.reload.images.first.image,
                       cover_photo: cover_photos.reload.images.first.image
@@ -163,10 +152,10 @@ puts 'done!'
 
 print 'Creating comments...'
 
-commentables = [Image, StatusUpdate]
+COMMENTABLES = [Image, StatusUpdate].freeze
 
-commentables.each do |commentable|
-  random_commentables = commentable.all.sample(commentable.count * 0.6)
+COMMENTABLES.each do |commentable|
+  random_commentables = commentable.all.sample(commentable.count * 0.7)
   random_commentables.each do |commentable_object|
     random_users =
       User.all
@@ -181,19 +170,20 @@ commentables.each do |commentable|
   end
 end
 
-print "#{Comment.count} created..."
-puts 'done!'
+puts "#{Comment.count} created!"
 
 # Create status update, comment and image likes
 print 'Creating likes...'
 
-likeables = [Image, StatusUpdate, Comment]
+LIKEABLES = [Image, StatusUpdate, Comment].freeze
 
-likeables.each do |likeable|
+LIKEABLES.each do |likeable|
   random_likeables = likeable.all.sample(likeable.count * 0.6)
 
   random_likeables.each do |likeable_object|
-    random_users = User.all.sample(rand(1..(MULTIPLIER * 0.4)))
+    random_users = User.all
+                       .sample(rand(1..(MULTIPLIER * 0.4)))
+                       .select { |user| likeable_object.author.friends.include? user }
 
     random_users.each do |user|
       likeable_object.likes.create(user: user)
@@ -201,7 +191,6 @@ likeables.each do |likeable|
   end
 end
 
-print "#{Like.count} created..."
-puts 'done!'
+puts "#{Like.count} created!"
 
 puts 'Done with everything!'
