e8f7a84985e6b5c9d765d39323c243395cf2cae9
Mario Zugaj <mario@mariozugaj.com>
2017-08-06 19:10:07 +0200

    Clean up application.rb paperclip config

--- config/application.rb
+++ config/application.rb
@@ -13,17 +13,5 @@ module PeopleBook
 
     # Add node modules to pipeline
     config.assets.paths << Rails.root.join('node_modules')
-
-    #paperclip S3
-    config.paperclip_defaults = {
-      storage: :s3,
-      s3_region: Figaro.env.AWS_S3_REGION,
-      s3_credentials: {
-        s3_host_name: Figaro.env.AWS_S3_HOST_NAME,
-        bucket: Figaro.env.AWS_S3_BUCKET,
-        access_key_id: Figaro.env.AWS_ACCESS_KEY_ID,
-        secret_access_key: Figaro.env.AWS_SECRET_ACCESS_KEY
-      }
-    }
   end
 end
