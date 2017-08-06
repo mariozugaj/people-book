45b0b0c5d378b578af8df23a84b0be99bec3a4e7
Mario Zugaj <mario@mariozugaj.com>
2017-08-06 20:08:43 +0200

    Fix new reg no redirect

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,9 +1,10 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   def create
-    super
-    Profile.create!(user: resource)
-    PhotoAlbum.create!(author: resource, name: 'Avatars')
-    PhotoAlbum.create!(author: resource, name: 'Cover photos')
-    UserMailer.welcome(resource).deliver_later
+    super do |resource|
+      Profile.create!(user: resource)
+      PhotoAlbum.create!(author: resource, name: 'Avatars')
+      PhotoAlbum.create!(author: resource, name: 'Cover photos')
+      UserMailer.welcome(resource).deliver_later
+    end
   end
 end
