956b8ca34447d7882913ff56ff5ac09050b6c4dd
Mario Zugaj <mario@mariozugaj.com>
2017-08-06 19:09:39 +0200

    Set facebook_callback env var

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -276,7 +276,7 @@ Devise.setup do |config|
   # config.omniauth_path_prefix = '/my_engine/users/auth'
 
   config.omniauth :facebook, Figaro.env.fb_app_id, Figaro.env.fb_app_secret,
-                  callback_url: 'http://localhost:3000/users/auth/facebook/callback',
+                  callback_url: Figaro.env.facebook_callback,
                   scope: 'email', display: 'page',
                   image_size: { width: 290, height: 290 },
                   secure_image_url: true,
