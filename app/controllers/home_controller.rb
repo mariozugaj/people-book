5a76c74b228b793da2f729223eae35bb4c1f7f59
Mario Zugaj <mario@mariozugaj.com>
2017-08-21 08:42:43 +0200

    Remove activity

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,16 +3,9 @@ class HomeController < ApplicationController
     @new_status_update ||= StatusUpdate.new
     @feed =
       StatusUpdate.includes([{ author: :profile }, :likers])
-                  .where(author: current_user.friends)
+                  .where(author: [current_user] << current_user.friends)
                   .ordered
                   .page(params[:page])
                   .per(6)
-    @activity =
-      Notification.includes([:recipient, :notifiable, actor: :profile])
-                  .where.not(actor: current_user)
-                  .where(actor: current_user.friends)
-                  .where('created_at > ?', 10.hours.ago)
-                  .order(created_at: :desc)
-                  .limit(10)
   end
 end
