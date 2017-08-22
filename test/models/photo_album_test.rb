a3e21ce5c8841edc1504ced675a0710612b275e9
Mario Zugaj <mario@mariozugaj.com>
2017-08-22 21:24:36 +0200

    Change photo album model test to reflect new method return

--- test/models/photo_album_test.rb
+++ test/models/photo_album_test.rb
@@ -9,7 +9,7 @@ class PhotoAlbumTest < ActiveSupport::TestCase
     assert @photo_album.valid?
   end
 
-  test 'return its first image' do
-    assert_equal 'lights.jpg', @photo_album.first_image.file.filename
+  test 'return its first image url' do
+    assert_equal '/uploads/image/309456473/image/mini_haha.jpg', @photo_album.first_image
   end
 end
