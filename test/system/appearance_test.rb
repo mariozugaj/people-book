57a7487b59926f1d5a8659d7f533db31edec2e87
Mario Zugaj <mario@mariozugaj.com>
2017-08-23 21:00:33 +0200

    Add appearances system test

---
+++ test/system/appearance_test.rb
@@ -0,0 +1,41 @@
+require 'application_system_test_case'
+
+class AppearanceTest < ApplicationSystemTestCase
+  include ActiveJob::TestHelper
+
+  setup do
+    @user = users :maymie
+    @friend = users :ronny
+    ActiveJob::Base.queue_adapter.perform_enqueued_jobs = true
+    RedisTest.start
+    RedisTest.configure(:default)
+  end
+
+  teardown do
+    RedisTest.clear
+    RedisTest.stop
+  end
+
+  test 'appearance indicator changes on login/logout' do
+    in_browser(:one) do
+      log_in_as @user
+      visit conversations_path
+      assert_selector "span.ui.empty.circular.label.user-#{@friend.slug}"
+    end
+
+    in_browser(:two) do
+      log_in_as @friend
+      visit conversations_path
+      assert_selector "span.ui.empty.circular.label.user-#{@user.slug}.green"
+    end
+
+    in_browser(:one) do
+      assert_selector "span.ui.empty.circular.label.user-#{@friend.slug}.green"
+      log_out
+    end
+
+    in_browser(:two) do
+      assert_selector "span.ui.empty.circular.label.user-#{@user.slug}"
+    end
+  end
+end
