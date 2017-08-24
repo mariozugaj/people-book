76da3fca555b88df580c8f1538217e41fd00b797
Mario Zugaj <mario@mariozugaj.com>
2017-08-24 09:59:09 +0200

    Temp remove search includes

--- app/services/search/category_search.rb
+++ app/services/search/category_search.rb
@@ -1,10 +1,10 @@
 module Search
   class CategorySearch
+    # TODO: add includes_per model after pull request #967
     def self.search(category_options = [], params)
       category_options[0].search params[:q],
                                  fields: [category_options[1]],
                                  match: :text_middle,
-                                 includes: category_options[2],
                                  page: params[:page],
                                  per_page: 12
     end
