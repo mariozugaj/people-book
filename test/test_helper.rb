80757241cc3f3f3ecaa8beacea34df3199a1343d
Mario Zugaj <mario@mariozugaj.com>
2017-08-24 20:52:42 +0200

    Clean up test_helper.rb, move carrierwave config to test env

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,30 +4,16 @@ require 'support/fixture_file_helpers'
 require 'support/omniauth_helpers'
 require 'support/integration_test_helpers'
 require 'support/system_test_helpers'
-require 'support/carrierwave_config'
 require 'minitest/autorun'
-
 
 class ActiveSupport::TestCase
   include OmniauthHelper
-  include CarrierwaveConfig
+  include SystemTestHelpers
+  include IntegrationTestHelpers
+
   fixtures :all
 
   OmniAuth.config.test_mode = true
-  Searchkick.disable_callbacks
-  Capybara.default_max_wait_time = 10
-end
-
-class ActionDispatch::IntegrationTest
-  include IntegrationTestHelpers
-  include CarrierwaveConfig
-  Searchkick.disable_callbacks
-  Capybara.default_max_wait_time = 10
-end
-
-class ActionDispatch::SystemTestCase
-  include SystemTestHelpers
-  include CarrierwaveConfig
   Searchkick.disable_callbacks
   Capybara.default_max_wait_time = 10
 end
