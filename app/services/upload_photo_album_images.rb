492cfa5f5bff1c37e007217c08eba6c324d3f79a
Mario Zugaj <mario@mariozugaj.com>
2017-08-24 19:56:13 +0200

    Minor changes to upload images service

--- app/services/upload_photo_album_images.rb
+++ app/services/upload_photo_album_images.rb
@@ -10,23 +10,23 @@ class UploadPhotoAlbumImages
 
   def execute
     upload_images unless params[:images].blank?
-    upload_cloud_image unless params[:cloud_image][:remote_image_url].blank?
+    upload_cloud_image unless (params[:cloud_image].blank? || params[:cloud_image][:remote_image_url].blank?)
     send_notification
   end
 
   def upload_images
     params[:images][:image].each do |image|
-      photo_album.images.create(image: image)
+      photo_album.images.create!(image: image)
     end
   end
 
   def upload_cloud_image
-    Image.create(remote_image_url: params[:cloud_image][:remote_image_url],
+    Image.create!(remote_image_url: params[:cloud_image][:remote_image_url],
                  photo_album: photo_album)
   end
 
   def send_notification
-    NotificationRelayJob.perform_later(photo_album.author.friends,
+    NotificationRelayJob.perform_later(photo_album.author.friends.to_a,
                                        photo_album.author,
                                        'shared',
                                        photo_album)
