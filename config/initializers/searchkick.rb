2e14778097450d6f20b33383bcf6ab5262ca01a8
Mario Zugaj <mario@mariozugaj.com>
2019-06-18 21:40:24 +0200

    Set ssl min_version to 0

--- config/initializers/searchkick.rb
+++ config/initializers/searchkick.rb
@@ -7,7 +7,7 @@ Searchkick.client =
     transport_options: {
       request: { timeout: 250 },
       ssl: {
-        min_version: OpenSSL::SSL::SSL3_VERSION,
+        min_version: 0,
         max_version: OpenSSL::SSL::TLS1_2_VERSION,
         ca_path: '/usr/lib/ssl'
       }
