fd22b37d137804e28d55bbe071ae74b8e4edeeb4
Mario Zugaj <mario@mariozugaj.com>
2019-06-18 20:25:57 +0200

    Set ssl min_version and ca_path

--- config/initializers/searchkick.rb
+++ config/initializers/searchkick.rb
@@ -4,8 +4,11 @@ Searchkick.client =
   Elasticsearch::Client.new(
     host: ENV['ELASTICSEARCH_URL'],
     retry_on_failure: true,
-    transport_options: { 
+    transport_options: {
       request: { timeout: 250 },
-      ssl: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
+      ssl: {
+        min_version: :TLSv1_2,
+        ca_path: '/usr/lib/ssl'
+      }
     }
   )
