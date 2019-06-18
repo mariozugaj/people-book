1bc9a63b183c6922ead6eb334475a60b41827d7b
Mario Zugaj <mario@mariozugaj.com>
2019-06-18 10:33:54 +0200

    Add ca_file path to searchkick initializer

--- config/initializers/searchkick.rb
+++ config/initializers/searchkick.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 
 Searchkick.client =
-  Elasticsearch::Client.new(hosts: ENV['ELASTICSEARCH_URL'],
-                            retry_on_failure: true,
-                            transport_options: { request: { timeout: 250 } })
+  Elasticsearch::Client.new(
+    hosts: ENV['ELASTICSEARCH_URL'],
+    retry_on_failure: true,
+    transport_options: { 
+      request: { timeout: 250 },
+      ssl: { ca_file: '/usr/lib/ssl/certs/ca-certificates.crt' } 
+    }
+  )
