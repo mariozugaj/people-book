81dc1d4220944b841489388d5a9be4988da2e4d5
Mario Zugaj <mario@mariozugaj.com>
2019-06-30 10:26:29 +0200

    Replace Bonsai with Searchbox

--- config/initializers/searchkick.rb
+++ config/initializers/searchkick.rb
@@ -2,10 +2,9 @@
 
 Searchkick.client =
   Elasticsearch::Client.new(
-    host: ENV['ELASTICSEARCH_URL'],
+    host: ENV['SEARCHBOX_SSL_URL'],
     retry_on_failure: true,
-    transport_options: { 
-      request: { timeout: 250 },
-      ssl: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
+    transport_options: {
+      request: { timeout: 250 }
     }
   )
